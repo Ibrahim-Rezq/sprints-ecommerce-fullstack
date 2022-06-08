5dff491ee85d3f799300425360ea392c58353136
unknown <ovic391@gmail.com>
2022-06-08 22:13:16 +0200

    Product Editing

--- Ecommerce/frontend/src/Redux/Features/Products/ProductsSlice.js
+++ Ecommerce/frontend/src/Redux/Features/Products/ProductsSlice.js
@@ -1,63 +1,61 @@
-import { createSlice } from '@reduxjs/toolkit'
-import { Products, Product } from '../../../Utils/Constant'
+import { createSlice } from '@reduxjs/toolkit';
+import { Products, Product } from '../../../Utils/Constant';
 
 const initialState = {
-    products: [],
-    product: {},
-    featuredProducts: [],
-    onSaleProducts: [],
-    // this will be used once connected to database
-    productsLoading: false,
-    productsError: false,
-    //
-}
+  products: [],
+  product: {},
+  featuredProducts: [],
+  onSaleProducts: [],
+  // this will be used once connected to database
+  productsLoading: false,
+  productsError: false,
+  //
+};
 
 export const productsSlice = createSlice({
-    name: 'products',
-    initialState,
-    reducers: {
-        getSingleProduct: (state, action) => {
-            state.productsLoading = false
-            state.product =
-                Product.filter((product) => {
-                    return product.id === action.payload
-                })[0] || {}
-            state.productsLoading = true
-        },
-        getFeaturedProducts: (state) => {
-            if (state.products.length > 0)
-                state.featuredProducts = [
-                    ...Products.filter((prod) => prod.featured),
-                ]
-        },
-        getOnSaleProducts: (state) => {
-            if (state.products.length > 0)
-                state.onSaleProducts = [
-                    ...Products.filter((prod) => prod.onSale),
-                ]
-        },
-        getProducts: (state) => {
-            if (state.products.length <= 0) state.products = [...Products]
-        },
-        addProduct: (state, action) => {
-            state.products.push(action.payload)
-        },
-        updateProduct: (state, action) => {
-            console.log(
-                `Product ${action.payload.name || 'Name Not Found'} Updated`
-            )
-        },
+  name: 'products',
+  initialState,
+  reducers: {
+    getSingleProduct: (state, action) => {
+      state.productsLoading = false;
+      state.product =
+        Product.filter((product) => {
+          return product.id === action.payload;
+        })[0] || {};
+      state.productsLoading = true;
     },
-})
+    getFeaturedProducts: (state) => {
+      if (state.products.length > 0)
+        state.featuredProducts = [...Products.filter((prod) => prod.featured)];
+    },
+    getOnSaleProducts: (state) => {
+      if (state.products.length > 0)
+        state.onSaleProducts = [...Products.filter((prod) => prod.onSale)];
+    },
+    getProducts: (state) => {
+      if (state.products.length <= 0) state.products = [...Products];
+    },
+    addProduct: (state, action) => {
+      state.products.push(action.payload);
+    },
+    updateSingleProduct: (state, action) => {
+      state.products = state.products.map((product) => {
+        if (product.id === action.payload.id) {
+          return { ...product, ...action.payload };
+        } else return product;
+      });
+    },
+  },
+});
 
 export const {
-    addProduct,
-    getSingleProduct,
-    getOnSaleProducts,
-    getFeaturedProducts,
-    getProducts,
-    updateProduct,
-} = productsSlice.actions
-export const productsState = (state) => state.products
+  addProduct,
+  getSingleProduct,
+  getOnSaleProducts,
+  getFeaturedProducts,
+  getProducts,
+  updateSingleProduct,
+} = productsSlice.actions;
+export const productsState = (state) => state.products;
 
-export default productsSlice.reducer
+export default productsSlice.reducer;
