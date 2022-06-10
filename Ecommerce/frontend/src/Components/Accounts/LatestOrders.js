db8c0fc6bb995d5a2e098d1b568ac59fd9ca4ba3
Seif Omran <seifomran1324@gmail.com>
2022-06-11 00:03:22 +0200

    Map Tables Array

--- Ecommerce/frontend/src/Components/Accounts/LatestOrders.js
+++ Ecommerce/frontend/src/Components/Accounts/LatestOrders.js
@@ -3,10 +3,30 @@ import { SingleOrder } from '../Orders';
 import { OrdersArray } from '../../Utils/Constant';
 
 function LatestOrders(props) {
+
+    const orders = OrdersArray.map((order)=>(
+        <tr>
+            <td>{order.orderId}</td>
+            <td>{order.price}</td>
+            <td>{order.orderDate}</td>
+            <td>{order.status}</td>
+        </tr>
+    ));
+
     return (
-        <div>
-            <SingleOrder order={OrdersArray[0]}></SingleOrder>
-        </div>
+        <table className="table">
+            <thead>
+                <tr>
+                    <th>Order ID</th>
+                    <th>Order Price</th>
+                    <th>Order Date</th>
+                    <th>Order Status</th>
+                </tr>
+            </thead>
+            <tbody>
+                {orders}
+            </tbody>
+        </table>
     );
 }
 
