ef288b6c41feaff4741a113144c94b86f7c4fb65
Seif Omran <seifomran1324@gmail.com>
2022-06-10 22:49:29 +0200

    Add user profile

---
+++ Ecommerce/frontend/src/Components/Accounts/Profile.js
@@ -0,0 +1,27 @@
+import React from "react";
+import classes from "./css/Profile.module.css";
+import ProfileImage from "./ProfileImage";
+import ProfileData from "./ProfileData";
+
+const DUMMY_USER = {
+  fname: "Seif",
+  lname: "Omran",
+  address: "Obour City",
+  phone: "01099662420",
+  profileImage:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Circle-icons-profile.svg/1200px-Circle-icons-profile.svg.png",
+};
+
+function Profile(props) {
+  return (
+    <div className={classes["user-profile"] + " container mt-5"}>
+      <div className="row">
+        <ProfileImage myClasses="col-xs-12 col-md-4" profileImage={DUMMY_USER.profileImage}></ProfileImage>
+        <ProfileData myClasses="col-xs-12 col-md-8" userData={DUMMY_USER}></ProfileData>
+        
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
